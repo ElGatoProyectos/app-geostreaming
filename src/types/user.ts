42e8a322427140d626977857acd2e09f8a9dcc7b
LilNait-S <style.damn.falling@gmail.com>
2024-06-21 12:58:34 -0500

    add user types

---
+++ src/types/user.ts
@@ -0,0 +1,23 @@
+import { ProductUpdateInType } from "./product";
+
+export interface UserInType {
+  ref_id?: number | null;
+  role_id: number;
+  full_name: string;
+  dni?: string | null;
+  phone: string;
+  email?: string | null;
+  balance: number;
+  username: string;
+  password: string;
+  enabled: string;
+}
+
+export interface UserOutType extends UserInType {
+  id: number;
+}
+
+export interface UserUpdateInType extends UserInType {
+  id: number;
+  products?: ProductUpdateInType[];
+}
