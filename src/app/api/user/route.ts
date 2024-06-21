658db5adb435d4d40af3ab28db0124bc37834114
LilNait-S <style.damn.falling@gmail.com>
2024-06-21 12:51:59 -0500

    add route user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,29 @@
+import { UserModel } from "@/models/mysql/user-model";
+import { UserService } from "@/service/user";
+import { NextRequest, NextResponse } from "next/server";
+
+const userService = new UserService({ userModel: UserModel });
+
+export async function GET() {
+  try {
+    const users = await userService.getAll();
+    return NextResponse.json(users);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Error to get Products" },
+      { status: 404 }
+    );
+  }
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const newUser = await userService.create({ req });
+    return NextResponse.json(newUser);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: "Error to Create Product" },
+      { status: 500 }
+    );
+  }
+}
