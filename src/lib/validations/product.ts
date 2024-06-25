abde4e8ae9fbf36eb4a08f219f3dc0f69fe83d8f
LilNait-S <style.damn.falling@gmail.com>
2024-06-25 10:09:22 -0500

    fix validation

--- src/lib/validations/product.ts
+++ src/lib/validations/product.ts
@@ -7,12 +7,7 @@ const accountSchema = z.object({
   pin: z.string(),
   numb_profiles: z.number(),
   numb_days_duration: z.number(),
-  status: z.string(),
-});
-
-const priceSchema = z.object({
-  role_id: z.number(),
-  price: z.number(),
+  status: z.enum(["AVAILABLE", "DELIVERED", "PENDING"]).optional(),
 });
 
 const platformSchema = z.object({
@@ -23,7 +18,8 @@ const platformSchema = z.object({
 const productSchema = z.object({
   platform: platformSchema,
   accounts: z.array(accountSchema).optional(),
-  prices: z.array(priceSchema),
+  price_in_cents: z.number(),
+  price_distributor_in_cents: z.number(),
 });
 
 export function validateProduct(productInfo: unknown) {
@@ -35,40 +31,26 @@ export function validateProduct(productInfo: unknown) {
   return { isValid: parseResult.success, productValidated: parseResult.data };
 }
 
-const productEditSchema = z.object({
+const productEditSchema = productSchema.extend({
   id: z.number(),
   platform_id: z.number(),
-  createdAt: z.string(),
-  platform: z.object({
+  created_at: z.string(),
+  updated_at: z.string(),
+  platform: platformSchema.extend({
     id: z.number(),
-    name: z.string(),
-    description: z.string(),
   }),
   accounts: z
     .array(
-      z.object({
+      accountSchema.extend({
         id: z.number().optional(),
-        is_active: z.boolean(),
-        email: z.string().email(),
-        password: z.string(),
-        pin: z.string(),
-        numb_profiles: z.number(),
-        numb_days_duration: z.number(),
-        status: z.string(),
         platform_id: z.number().optional(),
         product_id: z.number().optional(),
-        createdAt: z.string(),
+        created_at: z.string(),
+        updated_at: z.string(),
       })
     )
-    .optional(),
-  price: z.array(
-    z.object({
-      id: z.number().optional(),
-      price: z.string(),
-      product_id: z.number().optional(),
-      role_id: z.number(),
-    })
-  ),
+    .optional()
+    .nullable(),
 });
 
 export function validateEditedProduct(productInfo: unknown) {
