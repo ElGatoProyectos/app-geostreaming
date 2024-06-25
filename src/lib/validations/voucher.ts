8af7bfc74a1b9f33616908ebe5e5c2e3ccbe1e4a
LilNait-S <style.damn.falling@gmail.com>
2024-06-25 12:11:57 -0500

    add voucher validation

---
+++ src/lib/validations/voucher.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+const voucherSchema = z.object({
+  voucher_number: z.string().min(1),
+  value: z.number().int(),
+  date: z.string().datetime(),
+  user_id: z.number().int().nonnegative(),
+});
+
+export function validateVoucher(voucherInfo: unknown) {
+  const parseResut = voucherSchema.safeParse(voucherInfo);
+  if (!parseResut.success) {
+    throw new Error("Invalid Voucher info");
+  }
+  return parseResut.data;
+}
+
+const updateVoucher = voucherSchema.extend({
+  id: z.number().int().nonnegative(),
+});
+
+export function validateUpdateVoucher(voucherInfo: unknown) {
+  const parseResut = updateVoucher.safeParse(voucherInfo);
+  if (!parseResut.success) {
+    throw new Error("Invalid Voucher info");
+  }
+  return parseResut.data;
+}
