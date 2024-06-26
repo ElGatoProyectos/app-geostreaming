6df35dfcb812301ede189ce7e2ccbc7ad85c2789
LilNait-S <style.damn.falling@gmail.com>
2024-06-26 17:41:18 -0500

    add banace route

---
+++ src/lib/validations/balance.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+const balanceSchema = z.object({
+  balance_in_cents: z.number(),
+});
+
+export function validateBalance(orderInfo: unknown) {
+  const parseResut = balanceSchema.safeParse(orderInfo);
+  if (!parseResut.success) {
+    throw new Error("Invalid Order info");
+  }
+  return parseResut.data;
+}
