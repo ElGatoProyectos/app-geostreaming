29b55eb2db871ae7b9f72a97014033816ec171e6
LilNait-S <style.damn.falling@gmail.com>
2024-06-27 12:36:47 -0500

    fix

--- src/app/api/balance/[id]/route.ts
+++ src/app/api/balance/[id]/route.ts
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-
 import prisma from "@/lib/prisma";
 import { authOptions } from "../../auth-options";
 import { validateBalance } from "@/lib/validations/balance";
@@ -14,35 +13,84 @@ export async function PATCH(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  let session;
+
   try {
-    const session = await getServerSession(authOptions);
-
-    if (session.user.role !== "ADMIN") {
-      return NextResponse.json({ error: "forbidden" }, { status: 500 });
-    }
-
-    const balanceUser = await req.json();
-    const validatedBalanceUser = validateBalance(balanceUser);
-    const { balance_in_cents } = validatedBalanceUser;
-    const user = await prisma.user.update({
-      where: { id: Number(params.id) },
-      data: { balance_in_cents: { increment: balance_in_cents } },
-    });
-
-    const isDistributor = user.balance_in_cents >= CentsToBeADistributor;
-
-    const newRole = isDistributor ? distributor_role : user_role;
-
-    const updateUser = await prisma.user.update({
-      where: { id: Number(params.id) },
-      data: { role: newRole },
-    });
-
-    return NextResponse.json(updateUser);
-  } catch (error: any) {
+    session = await getServerSession(authOptions);
+  } catch (error) {
     return NextResponse.json(
-      { error: "Error to balance user" },
-      { status: 404 }
+      { error: "Error fetching session" },
+      { status: 500 }
     );
   }
+
+  if (session.user.role !== "ADMIN") {
+    return NextResponse.json({ error: "Forbidden" }, { status: 403 });
+  }
+
+  let balanceUser;
+  let validatedBalanceUser;
+  let accountId;
+
+  try {
+    accountId = Number(params.id);
+    if (isNaN(accountId)) {
+      return NextResponse.json(
+        { error: "Invalid account ID" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error processing account ID" },
+      { status: 400 }
+    );
+  }
+
+  try {
+    balanceUser = await req.json();
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+  }
+
+  try {
+    validatedBalanceUser = validateBalance(balanceUser);
+  } catch (error) {
+    return NextResponse.json({ error: "Validation error" }, { status: 400 });
+  }
+
+  let user;
+
+  try {
+    user = await prisma.user.update({
+      where: { id: accountId },
+      data: {
+        balance_in_cents: { increment: validatedBalanceUser.balance_in_cents },
+      },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error updating user balance" },
+      { status: 500 }
+    );
+  }
+
+  const isDistributor = user.balance_in_cents >= CentsToBeADistributor;
+  const newRole = isDistributor ? distributor_role : user_role;
+
+  let updateUser;
+
+  try {
+    updateUser = await prisma.user.update({
+      where: { id: accountId },
+      data: { role: newRole },
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error updating user role" },
+      { status: 500 }
+    );
+  }
+
+  return NextResponse.json(updateUser);
 }
