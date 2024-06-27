9b6ebdd12d25212ea2eccb81097d689d2276bb0f
LilNait-S <style.damn.falling@gmail.com>
2024-06-27 12:37:16 -0500

    fix

--- src/app/api/voucher/route.ts
+++ src/app/api/voucher/route.ts
@@ -7,9 +7,9 @@ export async function GET() {
   try {
     const vouchers = await prisma.voucher.findMany();
     return NextResponse.json(vouchers);
-  } catch (e) {
+  } catch (error: any) {
     return NextResponse.json(
-      { error: "Error to get vouchers" },
+      { error: "Error fetching vouchers", message: error.message },
       { status: 500 }
     );
   } finally {
@@ -21,13 +21,15 @@ export async function POST(req: NextRequest) {
   try {
     const voucherInfo = await req.json();
     const validatedVoucher = validateVoucher(voucherInfo);
+
     const newVoucher = await prisma.voucher.create({
       data: validatedVoucher,
     });
+
     return NextResponse.json(newVoucher);
-  } catch (e) {
+  } catch (error: any) {
     return NextResponse.json(
-      { error: "Error to create voucher" },
+      { error: "Error creating voucher", message: error.message },
       { status: 500 }
     );
   } finally {
