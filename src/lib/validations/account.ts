46939974832a5b19378e46bd261fb84872662539
LilNait-S <style.damn.falling@gmail.com>
2024-06-28 09:06:47 -0500

    add

--- src/lib/validations/account.ts
+++ src/lib/validations/account.ts
@@ -16,12 +16,26 @@ export function validateAccount(accountInfo: unknown) {
 
 const updateAccount = accountSchema.extend({
   id: z.number().int().nonnegative(),
+  renovation_date: z.string().datetime(),
   product_id: z.number().int().nonnegative(),
   platform_id: z.number().int().nonnegative(),
 });
 
 export function validateUpdateAccount(accountInfo: unknown) {
   const parseResut = updateAccount.safeParse(accountInfo);
+  if (!parseResut.success) {
+    throw new Error("Invalid Account info");
+  }
+  return parseResut.data;
+}
+
+export const accountRenovateSchema = z.object({
+  numb_days_duration: z.number(),
+  renovation_date: z.string(),
+});
+
+export function validateRenovateUpdateAccount(accountInfo: unknown) {
+  const parseResut = accountRenovateSchema.safeParse(accountInfo);
   if (!parseResut.success) {
     throw new Error("Invalid Account info");
   }
