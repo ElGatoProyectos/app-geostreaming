1f30318060b36113b8922143e1bcbe7af5ca01d2
LilNait-S <style.damn.falling@gmail.com>
2024-06-28 18:48:30 -0500

    add

---
+++ src/app/api/renovate/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/prisma";
+import { validateRenovate } from "@/lib/validations/renovate";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  let renovateInfo;
+  let renovateValidated;
+
+  try {
+    renovateInfo = await req.json();
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+  }
+
+  try {
+    renovateValidated = validateRenovate(renovateInfo);
+  } catch (error) {
+    return NextResponse.json({ error: "Validation error" }, { status: 400 });
+  }
+
+  try {
+    const newPlatform = await prisma.account.update({
+      where: { id: renovateValidated.account_id },
+      data: { renewal_date: new Date() },
+    });
+    return NextResponse.json(newPlatform);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error creating platform" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
