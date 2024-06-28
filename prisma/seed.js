5349b73eb48b314a3be1d15e51579cb7aa7207ad
LilNait-S <style.damn.falling@gmail.com>
2024-06-28 18:48:04 -0500

    add

--- prisma/seed.js
+++ prisma/seed.js
@@ -3,122 +3,171 @@ const bcrypt = require("bcrypt");
 
 const prisma = new PrismaClient();
 
-const initialProducts = [
+const initialAccounts = [
   {
+    email: "jasdsad@gmail.com",
+    password: "netf6541",
+    description: "description www.google.com",
+    pin: "1556",
+    status: "NOT_BOUGHT",
+    platform_id: 1, // harcodeado
+  },
+  {
+    email: "ana.perez@gmail.com",
+    password: "netf1234",
+    description: "description www.google.com",
+    pin: "1234",
+    status: "NOT_BOUGHT",
+    platform_id: 1, // harcodeado
+  },
+  {
+    email: "maria.hulu@gmail.com",
+    password: "hulu9987",
+    description: "description www.google.com",
+    pin: "7563",
+    status: "NOT_BOUGHT",
+    platform_id: 1, // harcodeado
+  },
+  {
+    email: "joh432u@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 2, // harcodeado
+  },
+  {
+    email: "joh432u@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 2, // harcodeado
+  },
+  {
+    email: "joh432u@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 2, // harcodeado
+  },
+  {
+    email: "joh432u@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 2, // harcodeado
+  },
+  {
+    email: "joh432u@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 2, // harcodeado
+  },
+  {
+    email: "jsffdslu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 4, // harcodeado
+  },
+  {
+    email: "heheheu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 3, // harcodeado
+  },
+  {
+    email: "heheheu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 3, // harcodeado
+  },
+  {
+    email: "heheheu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 3, // harcodeado
+  },
+  {
+    email: "heheheu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 3, // harcodeado
+  },
+  {
+    email: "heheheu@gmail.com",
+    password: "hulu4321",
+    description: "description www.google.com",
+    pin: "9181",
+    status: "NOT_BOUGHT",
+    platform_id: 3, // harcodeado
+  },
+];
+
+const initialPlatforms = [
+  {
+    img_url:
+      "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    name: "NETFLIX INTERNACIONAL COMPLETA 1 MES",
+    description: "descripcion obligada o opcional?",
+    days_duration: 30,
+    price_in_cents: 1250,
+    price_distributor_in_cents: 1100,
+    status: "IMMEDIATE_DELIVERY",
+  },
+  {
+    img_url:
+      "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    name: "1 PERFIL ORIGINAL NETFLIX 1 MES",
+    description: "descripcion obligada o opcional?",
+    days_duration: 30,
+    price_in_cents: 250,
+    price_distributor_in_cents: 350,
+    status: "IMMEDIATE_DELIVERY",
+  },
+  {
+    img_url:
+      "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    name: "MAX INTERNACIONAL COMPLETA 1 MES",
+    description: "descripcion obligada o opcional?",
+    days_duration: 30,
     price_in_cents: 300,
-    price_distributor_in_cents: 200,
-    status: "UPON_REQUEST",
-    platform: {
-      img_url:
-        "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
-      name: "Netflix",
-      description:
-        "Streaming service with a wide variety of TV shows, movies, anime, documentaries, and more.",
-    },
-    accounts: [
-      {
-        email: "jasdsad@gmail.com",
-        password: "netf6541",
-        pin: "1556",
-        numb_profiles: 4,
-        numb_days_duration: 30,
-      },
-      {
-        email: "ana.perez@gmail.com",
-        password: "netf1234",
-        pin: "1234",
-        numb_profiles: 4,
-        numb_days_duration: 30,
-      },
-    ],
-  },
-  {
-    price_in_cents: 350,
-    price_distributor_in_cents: 310,
-    status: "UPON_REQUEST",
-    platform: {
-      img_url:
-        "https://greenhouse.hulu.com/app/uploads/sites/12/2023/10/networkID-hulu.png",
-      name: "Hulu",
-      description:
-        "Streaming service offering live and on-demand TV and movies, with and without commercials.",
-    },
-    accounts: [
-      {
-        email: "maria.hulu@gmail.com",
-        password: "hulu9987",
-        pin: "5678",
-        description:
-          "Links de descarga: https://www.google.com/ , https://www.google.com/ , https://www.google.com/ , https://www.google.com/ ",
-        numb_profiles: 5,
-        numb_days_duration: 30,
-      },
-      {
-        email: "john.hulu@gmail.com",
-        password: "hulu4321",
-        description:
-          "Links de descarga: https://www.google.com/ , https://www.google.com/ , https://www.google.com/ , https://www.google.com/ ",
-        pin: "8765",
-        numb_profiles: 5,
-        numb_days_duration: 30,
-      },
-    ],
-  },
-  {
-    price_in_cents: 120,
-    price_distributor_in_cents: 50,
+    price_distributor_in_cents: 400,
     status: "IMMEDIATE_DELIVERY",
-    platform: {
-      img_url:
-        "https://www.infobae.com/new-resizer/n1UXX1TKux_6oeaGxLLs7cnLnno=/1200x675/filters:format(webp):quality(85)/cloudfront-us-east-1.images.arcpublishing.com/infobae/5EGT4P4UKRGAZPDR52FKJJW4YU.png",
-      name: "Disney+",
-      description:
-        "Streaming service offering Disney, Pixar, Marvel, Star Wars, and National Geographic content.",
-    },
-    accounts: [
-      {
-        email: "alice.disney@gmail.com",
-        password: "disney4321",
-        pin: "1111",
-        numb_profiles: 7,
-        numb_days_duration: 30,
-      },
-      {
-        email: "bob.disney@gmail.com",
-        password: "disney5678",
-        pin: "2222",
-        numb_profiles: 7,
-        numb_days_duration: 30,
-      },
-    ],
   },
   {
-    price_in_cents: 195,
-    price_distributor_in_cents: 30,
+    img_url:
+      "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    name: "1 PERFIL MAX 1 MES",
+    description: "descripcion obligada o opcional?",
+    days_duration: 30,
+    price_in_cents: 180,
+    price_distributor_in_cents: 140,
     status: "IMMEDIATE_DELIVERY",
-    platform: {
-      img_url:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Amazon_Prime_Video_blue_logo_2.svg/2560px-Amazon_Prime_Video_blue_logo_2.svg.png",
-      name: "Amazon Prime Video",
-      description:
-        "Streaming service with a wide range of popular movies, TV shows, and original content.",
-    },
-    accounts: [
-      {
-        email: "charles.amazon@gmail.com",
-        password: "prime9988",
-        pin: "3333",
-        numb_profiles: 6,
-        numb_days_duration: 30,
-      },
-      {
-        email: "diana.amazon@gmail.com",
-        password: "prime1234",
-        pin: "4444",
-        numb_profiles: 6,
-        numb_days_duration: 30,
-      },
-    ],
+  },
+  {
+    img_url:
+      "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940",
+    name: "AMAZON PRIME VIDEO COMPLETA 1 MES",
+    description: "descripcion obligada o opcional?",
+    days_duration: 30,
+    price_in_cents: 400,
+    price_distributor_in_cents: 350,
+    status: "IMMEDIATE_DELIVERY",
   },
 ];
 
@@ -128,6 +177,7 @@ const users = [
     full_name: "pedro perez diaz",
     dni: "52668846",
     phone: "111222333",
+    country_code: "+42",
     password: "123456",
   },
   {
@@ -135,6 +185,7 @@ const users = [
     full_name: "Juan martinez",
     dni: "97484661",
     phone: "77788899",
+    country_code: "+85",
     password: "123456",
   },
 ];
@@ -145,6 +196,7 @@ const admin = [
     password: "123456789",
     full_name: "admin joel",
     phone: "555666111",
+    country_code: "+51",
   },
 ];
 
@@ -173,49 +225,21 @@ async function main() {
     data: usersEncryptedPass,
   });
 
-  for (const product of initialProducts) {
+  for (const platform of initialPlatforms) {
     const newPlatform = await prisma.platform.create({
-      data: product.platform,
+      data: platform,
     });
 
-    const newProduct = await prisma.product.create({
-      data: {
-        platform_id: newPlatform.id,
-        price_distributor_in_cents: product.price_distributor_in_cents,
-        price_in_cents: product.price_in_cents,
-        status: product.status,
-      },
-    });
-
-    let newAccounts = null;
-    if (product.accounts) {
-      const accountsWithProductAndPlatformId = product.accounts.map(
-        (account) => ({
-          ...account,
-          platform_id: newPlatform.id,
-          product_id: newProduct.id,
-        })
-      );
-
-      await prisma.account.createMany({
-        data: accountsWithProductAndPlatformId,
-      });
-
-      newAccounts = await prisma.account.findMany({
-        where: { product_id: newProduct.id },
-      });
-    }
-
-    const newProductOut = {
-      ...newProduct,
-      platform: newPlatform,
-      accounts: newAccounts,
-    };
     await prisma.$disconnect();
-    console.log(`Created product with id: ${newProduct.id}`);
-    console.log(`Created product: ${newProductOut}`);
+    console.log(`Created platform with id: ${newPlatform.id}`);
   }
 
+  for (const account of initialAccounts) {
+    const newAccount = await prisma.account.create({
+      data: account,
+    });
+    console.log(`Created account with id: ${newAccount.id}`);
+  }
   console.log("Seeding finished.");
 }
 main()
