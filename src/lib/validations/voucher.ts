36618575ec32f81c6fc39e738eb96b3ef684a35c
LilNait-S <style.damn.falling@gmail.com>
2024-06-29 11:16:56 -0500

    add

--- src/lib/validations/voucher.ts
+++ src/lib/validations/voucher.ts
@@ -2,10 +2,11 @@ import { z } from "zod";
 
 const voucherSchema = z.object({
   voucher_url: z.string(),
-  voucher_number: z.string().min(1),
+  number: z.string().min(1),
   value: z.number().int(),
   date: z.string().datetime(),
   user_id: z.number().int().nonnegative(),
+  country_code: z.string(),
 });
 
 export function validateVoucher(voucherInfo: unknown) {
