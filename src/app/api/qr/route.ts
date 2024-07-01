ef0f756cedfd324ad025e2a9e02f8054676e63b1
LilNait-S <style.damn.falling@gmail.com>
2024-07-01 14:56:49 -0500

    add

--- src/app/api/qr/route.ts
+++ src/app/api/qr/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
@@ -8,24 +8,16 @@ export async function GET() {
     };
 
     const res = await fetch(url_wsp, options);
-    console.log("res", res);
-    // Usamos.blob() para obtener los datos de la imagen como un Blob
-    const imgBlob = await res.blob();
 
-    // Convertimos el Blob a una cadena Base64
-    const reader = new FileReader();
-    let base64data;
-    reader.onloadend = () => {
-      base64data = reader.result;
-      console.log("Base64 Image Data:", base64data); // Aquí puedes usar la imagen como desees
-    };
-    reader.readAsDataURL(imgBlob);
+    if (!res.ok) {
+      throw new Error("Failed to fetch QR code");
+    }
 
-    // Otra opción es crear una URL de objeto para el Blob y usarla directamente en un elemento <img>
-    const imgUrl = URL.createObjectURL(imgBlob);
-    console.log("Image URL:", imgUrl); // Esta URL puede ser asignada a src de un elemento <img>
+    const buffer = await res.arrayBuffer();
+    const base64Image = Buffer.from(buffer).toString("base64");
+    const imageType = res.headers.get("content-type");
 
-    return NextResponse.json({ qr: "qr", photo: base64data });
+    return NextResponse.json({ qr: `data:${imageType};base64,${base64Image}` });
   } catch (error: any) {
     return NextResponse.json({ error: "Error to get QR" }, { status: 500 });
   }
