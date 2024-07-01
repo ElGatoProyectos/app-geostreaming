1f5d3bf3ed7667b21dbe33835613737b4f5a93bb
LilNait-S <style.damn.falling@gmail.com>
2024-07-01 17:58:01 -0500

    fix

--- src/app/api/platform/route.ts
+++ src/app/api/platform/route.ts
@@ -1,8 +1,6 @@
 import prisma from "@/lib/prisma";
 import { validatePlatform } from "@/lib/validations/platform";
 import { NextRequest, NextResponse } from "next/server";
-import path from "path";
-import { writeFile } from "fs/promises";
 
 export async function GET() {
   try {
@@ -21,51 +19,24 @@ export async function GET() {
 }
 
 export async function POST(req: NextRequest) {
-  let data;
-  data = await req.formData();
+  let platformInfo;
+  let platformValidated;
 
-  const {
-    file,
-    name,
-    description,
-    days_duration,
-    price_distributor_in_cents,
-    status,
-  } = Object.fromEntries(data.entries()) as {
-    file: File;
-    name: string;
-    description: string;
-    days_duration: string;
-    price_distributor_in_cents: string;
-    status: "IMMEDIATE_DELIVERY" | "UPON_REQUEST";
-  };
+  try {
+    platformInfo = await req.json();
+  } catch (error) {
+    return NextResponse.json({ error: "Invalid JSON" }, { status: 400 });
+  }
 
-  const update = {
-    name,
-    description,
-    days_duration,
-    price_distributor_in_cents,
-    status,
-  };
-  let validatedPlatform = validatePlatform(update);
-  let newPlatform;
-
-  if (file) {
-    newPlatform = await prisma.platform.create({
-      data: validatedPlatform,
-    });
-    await prisma.$disconnect();
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
-    const nameImage = "platforms_" + newPlatform.id + ".png";
-    const filePath = path.join(process.cwd(), `public/platforms`, nameImage);
-    await writeFile(filePath, buffer);
-    return NextResponse.json(newPlatform);
+  try {
+    platformValidated = validatePlatform(platformInfo);
+  } catch (error) {
+    return NextResponse.json({ error: "Validation error" }, { status: 400 });
   }
 
   try {
     const newPlatform = await prisma.platform.create({
-      data: validatedPlatform,
+      data: platformValidated,
     });
     await prisma.$disconnect();
     return NextResponse.json(newPlatform);
