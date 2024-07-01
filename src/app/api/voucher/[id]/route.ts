97fd8b0c3865734f9e4ee83787ad84697631dd88
LilNait-S <style.damn.falling@gmail.com>
2024-07-01 14:56:58 -0500

    add

--- src/app/api/voucher/[id]/route.ts
+++ src/app/api/voucher/[id]/route.ts
@@ -1,6 +1,8 @@
 import prisma from "@/lib/prisma";
 import { validateVoucher } from "@/lib/validations/voucher";
 import { NextRequest, NextResponse } from "next/server";
+import path from "path";
+import { writeFile } from "fs/promises";
 
 export async function GET(
   _: NextRequest,
@@ -11,6 +13,7 @@ export async function GET(
     const foundVoucher = await prisma.voucher.findUnique({
       where: { id: voucher_id },
     });
+    prisma.$disconnect();
     return NextResponse.json(foundVoucher);
   } catch (e) {
     return NextResponse.json(
@@ -26,16 +29,76 @@ export async function PATCH(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  let voucher_id;
+
   try {
-    const voucher_id = Number(params.id);
-    const voucherInfo = await req.json();
-    const validatedVoucher = validateVoucher(voucherInfo);
-    const updatedVoucher = await prisma.voucher.update({
+    voucher_id = Number(params.id);
+    if (isNaN(voucher_id)) {
+      return NextResponse.json(
+        { error: "Invalid voucher ID" },
+        { status: 400 }
+      );
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Error processing voucher ID" },
+      { status: 400 }
+    );
+  }
+  try {
+    let data;
+    data = await req.formData();
+
+    const { file, number, value, date, user_id, country_code } =
+      Object.fromEntries(data.entries()) as {
+        file: File;
+        number: string;
+        value: string;
+        date: string;
+        user_id: string;
+        country_code: string;
+      };
+
+    const updateUser = {
+      number,
+      value,
+      date,
+      user_id,
+      country_code,
+    };
+    const validatedVoucher = validateVoucher(updateUser);
+    let newVoucher;
+    if (file) {
+      newVoucher = await prisma.voucher.update({
+        where: { id: voucher_id },
+        data: validatedVoucher,
+      });
+      prisma.$disconnect();
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const nameImage = "vouchers_" + newVoucher.id + ".png";
+      const filePath = path.join(process.cwd(), `public/vouchers`, nameImage);
+      await writeFile(filePath, buffer);
+      return NextResponse.json(newVoucher);
+    }
+
+    newVoucher = await prisma.voucher.update({
       where: { id: voucher_id },
       data: validatedVoucher,
     });
+    prisma.$disconnect();
 
-    return NextResponse.json(updatedVoucher);
+    return NextResponse.json(newVoucher);
+
+    // const voucher_id = Number(params.id);
+    // const voucherInfo = await req.json();
+    // const validatedVoucher = validateVoucher(voucherInfo);
+    // const updatedVoucher = await prisma.voucher.update({
+    //   where: { id: voucher_id },
+    //   data: validatedVoucher,
+    // });
+    // prisma.$disconnect();
+    // return NextResponse.json(updatedVoucher);
   } catch (error: any) {
     return NextResponse.json(
       { error: "Error to update voucher" },
@@ -55,6 +118,7 @@ export async function DELETE(
     await prisma.voucher.delete({
       where: { id: voucher_id },
     });
+    prisma.$disconnect();
 
     return NextResponse.json({ message: "deleted voucher" });
   } catch (e) {
