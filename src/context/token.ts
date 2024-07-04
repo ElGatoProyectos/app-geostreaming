9f81cae4dd0bef518aebbc2cd1930332114a9242
LilNait-S <style.damn.falling@gmail.com>
2024-07-04 15:05:45 -0500

    add

--- src/context/token.ts
+++ src/context/token.ts
@@ -1,11 +1,11 @@
-export const dev = true;
+export const dev = false;
 
 const { NEXT_PUBLIC_BACKEND_URL } = process.env;
 
 export const url_backend = NEXT_PUBLIC_BACKEND_URL
   ? NEXT_PUBLIC_BACKEND_URL
-  : "http://161.132.37.105";
+  : "http://161.132.37.105:4000";
 
 export const url_front_to_wsp = dev
   ? "http://localhost:4000"
-  : "http://161.132.37.105";
+  : "http://161.132.37.105:4000";
