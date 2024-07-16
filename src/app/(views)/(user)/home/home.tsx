d18347fe908f92a57f0a40a3f8b2b05b488c859e
HansM7 <melchorhans5@gmail.com>
2024-07-15 20:18:16 -0500

    chage visualization modal

--- src/app/(views)/(user)/home/home.tsx
+++ src/app/(views)/(user)/home/home.tsx
@@ -28,7 +28,7 @@ const Home = () => {
     <div className="flex flex-col gap-8">
       <Request />
       <Delivery />
-      {session.data?.user.role === "DISTRIBUTOR" && (
+      {session.data?.user.role !== "ADMIN" && (
         <Modal title="Avisos" isOpen={openModal} onClose={closeModal}>
           <div className="text-center border-[#F2308B] border p-4 rounded-lg text-[#444]  flex flex-col justify-center items-center gap-8">
             <h2 className=" text-xl font-semibold ">
