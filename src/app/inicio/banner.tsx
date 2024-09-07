c290a4b07672b02367776289c093758ac210ba2a
DevDanielaCN <dani.cn@outlook.es>
2024-09-06 21:58:04 -0500

    update contamos in banner

--- src/app/inicio/banner.tsx
+++ src/app/inicio/banner.tsx
@@ -15,7 +15,7 @@ const banner = () => {
         <div className='w-full h-full flex justify-center items-center md:justify-start max-w-[1440px] mx-auto px-4 md:px-8'>
         <div className='md:max-w-[50%] flex flex-col items-center md:items-start gap-4 text-center md:text-start w-[90%] ' data-aos="zoom-out">
           <h2 className='text-white font-bold text-[1.6rem] md:text-[3rem] drop-shadow-lg' >Todo el entretenimiento en un solo lugar</h2>
-          <p className='text-white drop-shadow-lg md:text-[2rem]' >Contamnos con una gran variedad de productos</p>
+          <p className='text-white drop-shadow-lg md:text-[2rem]' >Contamos con una gran variedad de productos</p>
           <Link href={'/ingresar'} className='text-white bg-[#F2308B] border-2 border-[#F2308B] rounded  px-6 py-2 text-lg hover:bg-transparent hover:border-white transition-all duration-300' >Ingresa</Link>
         </div>
         </div>
